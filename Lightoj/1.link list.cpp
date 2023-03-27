f8f877895d05c4f66c33b1174ad5002a223ca70b
mahadi_naeem <mahadinaeem00@gmail.com>
2023-03-27 17:36:17 +0600

    naeem

---
+++ Lightoj/1.link list.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *Next;
+};
+void display(Node *n)
+{
+    while (n != NULL)
+    {
+        cout << n->value;
+        if (n->Next != NULL)
+            cout << "->";
+        n = n->Next;
+    }
+    cout << endl
+         << endl;
+}
+
+int main()
+{
+    Node *head = new Node();
+    Node *second = new Node();
+    Node *third = new Node();
+    Node *fourth = new Node();
+
+    head->value = 1;
+    second->value = 8;
+    third->value = 10;
+    fourth->value = 20;
+
+    head->Next = second;
+    second->Next = third;
+    third->Next = fourth;
+    fourth->Next = NULL;
+
+    display(head);
+
+    return 0;
+}
