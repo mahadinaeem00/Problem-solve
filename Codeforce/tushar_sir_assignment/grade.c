6756cfb4024364688205f94215678c2b01ba07ff
mahadi_naeem <mahadinaeem00@gmail.com>
2023-03-29 05:02:24 +0600

    grad

---
+++ Codeforce/tushar_sir_assignment/grade.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("Enter the Number:");
+    scanf("%d",&n);
+
+    if(n>=80 && n<=100)
+       printf("He got A+");
+
+    else if(n>=70)
+       printf("He got A");
+
+    else if(n>=60)
+       printf("He got A-");
+
+    else if(n>=50)
+      printf("He got B");
+
+    else if(n>40)
+       printf("He got C");
+
+    else if(n>=33)
+       printf("He got D");
+
+    else if(n>=0 && n<33)
+       printf("He failed in the exam");
+}
