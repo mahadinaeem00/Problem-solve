e41a372423c91245518679ef2bc94ab4945ac3f0
mahadi_naeem <mahadinaeem00@gmail.com>
2023-03-29 07:26:37 +0600

    grad

---
+++ tushar_sir_assignment/grade_by_switch.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the marks:");
+    scanf("%d",&n);
+    int num=n/10;
+
+    switch(n>=0 && n<=10)
+    {
+           case value 1:
+           printf("FAil");
+           break;
+    }
+}
