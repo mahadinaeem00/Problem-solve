1f3401765ee82473c0a09d747e8e84bde63347ce
mahadinaeem <mahadinaeem00@gmail.com>
2023-04-01 19:13:28 +0600

    c

---
+++ linked_list/link_list_improvement.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *Next;
+
+    Node(int val)
+    {
+        value = val;
+        Next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = NULL;
+}
